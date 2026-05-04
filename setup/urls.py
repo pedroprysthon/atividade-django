0cc215392a3c858412608e624b727a225af16676
PedroPrysthon <joaopedroprysthon@gmail.com>
2026-05-03 21:11:30 -0300

    ajustes

--- setup/urls.py
+++ setup/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import hello # Importe a função que você criou
+from core.views import hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
